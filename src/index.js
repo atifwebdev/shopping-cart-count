b521a73744f98696fe4cbc5e2c75339ffc259348
Muhammad Atif <m.atifwebdeveloper@gmail.com>
2023-11-22 17:29:38 +0500

    fix deploy

--- src/index.js
+++ src/index.js
@@ -6,14 +6,5 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
